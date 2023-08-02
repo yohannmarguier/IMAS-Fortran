a173cc554146ae2751bedd18cd867f5a3ac9b064
Olivier Hoenen <Olivier.Hoenen@iter.org>
2023-08-02 23:15:54 +0200

    Rename ual->al in documentation

--- doc/code_samples/imas_hello_world.f90
+++ doc/code_samples/imas_hello_world.f90
@@ -3,11 +3,11 @@ program imas_hello_world
     use ids_routines
     implicit none
 
-    CHARACTER(len=255) :: ual_version
+    CHARACTER(len=255) :: al_version
 
-    call get_environment_variable("UAL_VERSION", ual_version)
+    call get_environment_variable("AL_VERSION", al_version)
 
     write(*,*) "Hello world!"
-    write(*,*) "Using access layer version: ", ual_version
+    write(*,*) "Using access layer version: ", al_version
 
 end program imas_hello_world
