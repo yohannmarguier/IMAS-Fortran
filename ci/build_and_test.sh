645621de5789a9c9a1f6f914bde368cd9b1760ec
Maarten Sebregts <msebregts@ignitioncomputing.com>
2024-02-06 15:41:06 +0100

    Use test database inside the CI build folder

--- ci/build_and_test.sh
+++ ci/build_and_test.sh
@@ -68,6 +68,9 @@ cmake -B build "${CMAKE_ARGS[@]}"
 # Build
 make -C build -j8 all
 
+# Create test database, point USER env variable to the test database
+rm -rf testdb
+export USER="$(pwd)/testdb"
 # Test
 export ARGS="--output-on-failure --output-junit ctest.xml"
 make -C build test
