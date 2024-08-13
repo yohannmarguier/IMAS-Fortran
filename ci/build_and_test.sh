c73305d4c9792550656477b2ec40dab27c485d37
Olivier Hoenen <Olivier.Hoenen@iter.org>
2024-08-13 18:07:08 +0200

    Revert "[TEMP] Use feature branch of al-core for CI testing"
    This reverts commit f9b008af970ddb55e66628c871a9c7cd1d495b2e.

--- ci/build_and_test.sh
+++ ci/build_and_test.sh
@@ -128,7 +128,6 @@ CMAKE_ARGS=(${CMAKE_ARGS[@]}
     -DAL_DOWNLOAD_DEPENDENCIES=ON
     -DAL_COMMON_GIT_REPOSITORY=https://git.iter.org/scm/imas/al-common.git
     -DAL_CORE_GIT_REPOSITORY=https://git.iter.org/scm/imas/al-core.git
-    -DAL_CORE_VERSION=feature/improved-serialization
     -DAL_PLUGINS_GIT_REPOSITORY=https://git.iter.org/scm/imas/al-plugins.git
     -DDD_GIT_REPOSITORY=https://git.iter.org/scm/imas/data-dictionary.git
     # DD version: can be set with DD_VERSION env variable, otherwise use latest master/3
